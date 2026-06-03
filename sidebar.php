4e99984095af0de20aaa5933a2d6bf288c2f4320
Dikaaa <160558340+trasherss@users.noreply.github.com>
2026-06-03 15:44:02 +0700

    sidebar admin

---
+++ sidebar.php
@@ -0,0 +1,41 @@
+<div class="main-content">
+
+    <div class="topbar">
+        <span class="admin">
+            <a>Admin</a>
+        </span>
+    </div>
+
+<div class="sidebar">
+    <div class="sidebar-header">
+        <h2>CATERING</h2>
+    </div>
+
+    <ul class="sidebar-menu">
+        <li>
+            <a href="dashboard.php" class="active">
+                 Dashboard
+            </a>
+        </li>
+        <li>
+            <a href="menu.php">
+                Menu
+            </a>
+        </li>
+        <li>
+            <a href="kategori.php">
+                 Kategori
+            </a>
+        </li>
+        <li>
+            <a href="pesanan.php">
+                 Pesanan
+            </a>
+        </li>
+        <li>
+            <a href="?logout">
+                 Logout
+            </a>
+        </li>
+    </ul>
+</div>
